656c6cf5ccd950aa638df6a82128efadb5dd94da
Anderson Tavares <nocturne.pe@gmail.com>
2016-01-14 10:35:09 -0300

    Adding Tests Support

--- cmake/AtMacros.cmake
+++ cmake/AtMacros.cmake
@@ -20,3 +20,30 @@ function(at_cat IN_FILE OUT_FILE lines_to_remove)
   endforeach()
   file(APPEND ${OUT_FILE} "${CONTENTS2}")
 endfunction()
+# ------------------
+# Set compilation standard to C11
+# ------------------
+macro(at_use_c11)
+  if (CMAKE_VERSION VERSION_LESS "3.1")
+    if (CMAKE_C_COMPILER_ID STREQUAL "GNU")
+      set (CMAKE_C_FLAGS "--std=c11 ${CMAKE_C_FLAGS}")
+    endif ()
+  else ()
+    set (CMAKE_C_STANDARD 11)
+  endif ()
+endmacro(at_use_c11)
+# ------------------
+# Create the tests for a module
+# @param module the name of the module
+# ------------------
+macro(at_create_tests module)
+  string(TOUPPER ${module} MODULEU)
+  at_use_c11()
+  IF(BUILD_${MODULEU})
+    FOREACH(TEST ${AT_${MODULEU}_TESTS})
+      add_executable       (test_${TEST} tests/${module}/test_${TEST}.c)
+      target_link_libraries(test_${TEST} ${AT_${MODULEU}_LIBRARIES} cmocka)
+      add_test             (test_${TEST} test_${TEST})
+    ENDFOREACH()
+  ENDIF()
+endmacro()
---
+++ tests/core/test_array.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <setjmp.h>
+#include <stdarg.h>
+#include <cmocka.h>
+int main(int argc, char** argv){
+  return 0;
+}
